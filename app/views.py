d325defe068e6bcf25f649fed0c56bf394ce45da
Philip <philiprying@protonmail.com>
2021-11-12 13:50:13 +0100

    bootstrap

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 import hashlib
 
 from pylearn.settings import SIGNING_SALT
@@ -6,16 +6,25 @@ from pylearn.settings import SIGNING_SALT
 
 def login(request):
 
-    try:
-        username = request.POST['username']
-        password = hashlib.sha256(str(request.POST['password']+SIGNING_SALT).encode('utf8')).hexdigest()
-        print(username)
-        print(password)
-    except Exception as e:
-        print(e)
-        username = ""
-        password = ""
+    if request.method == 'POST':
+        try:
+            username = request.POST['username']
+            password = hashlib.sha256(str(request.POST['password']+SIGNING_SALT).encode('utf8')).hexdigest()
+            print(username)
+            print(password)
+        except Exception as e:
+            print(e)
+            username = ""
+            password = ""
+        # check if user is valid if it is redirect
+        if username != '':
+            return redirect('https://google.com')
+    
+        #context = {'username': username, 'password': password}
+    
+    return render(request, 'login.html')
 
-    context = {'username': username, 'password': password}
 
-    return render(request, 'login.html', context)
+    
+
+    
