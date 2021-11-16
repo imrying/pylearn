ac1a773d31c5722719cf1b9871ee9df0de49d54f
Philip <philiprying@protonmail.com>
2021-11-16 10:40:37 +0100

    initial db commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.core.validators import int_list_validator
+from django.db.models.fields import CharField
+from django.db.models.fields.files import FileField
 
 # Create your models here.
+
+class Student(models.Model):
+    username = models.CharField(max_length=200, unique=True)
+    password_hash = models.CharField(max_length=200)
+
+class Teacher(models.Model):
+    username = models.CharField(max_length=200, unique=True)
+    password_hash = models.CharField(max_length=200)
+
+class SchoolClass(models.Model):
+    class_name = models.CharField(max_length=200, unique=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    students = models.ManyToManyField(Student)
+
+class Assignment(models.Model):
+    assignment_name = models.CharField(max_length=200)
+    school_class = models.ForeignKey(SchoolClass, on_delete=models.CASCADE)
+
+class Test(models.Model):
+    test_cases = FileField()
+    assignment = models.ForeignKey(Assignment,on_delete=models.CASCADE)
+
+class TestCase(models.Model):
+    test = models.ForeignKey(Test,on_delete=models.CASCADE)
+    input = CharField(max_length=200)
+    output = CharField(max_length=200)
+
+
+
