2db761adf2c86d03259d4661b83a54aea686d99e
Philip <philiprying@protonmail.com>
2021-11-16 12:12:26 +0100

    relational databases

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+
+from .models import *
+
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(SchoolClass)
+admin.site.register(Assignment)
+admin.site.register(Test)
+admin.site.register(TestCase)
+
+
+
 # Register your models here.
